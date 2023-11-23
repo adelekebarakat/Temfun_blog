17da4fcdbba1a3849fc5f2f2e2bf92fbf06bc401
Adeleke Barakat <adelekebarakat96@gmail.com>
2023-11-23 11:51:32 +0100

    unable to makemigrations

--- blog/settings.py
+++ blog/settings.py
@@ -34,7 +34,7 @@ environ.Env.read_env()
 # DEBUG = False
 
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 
 ALLOWED_HOSTS = ['*']
